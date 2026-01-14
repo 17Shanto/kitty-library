5403c1589d7050125525f672303d7b5e4717a233
17Shanto <shahporanshanto23@gmail.com>
2026-01-14 18:43:48 +0600

    migrate: all models to db module

---
+++ src/db/models.py
@@ -0,0 +1,58 @@
+from typing import Optional, List
+from sqlmodel import SQLModel, Field, Column, Relationship
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime, date
+import uuid 
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books" #type: ignore
+    uid: uuid.UUID = Field(
+        sa_column= Column(
+            pg.UUID,
+            nullable = False,
+            primary_key =True,
+            default=uuid.uuid4
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: date
+    page_count: int
+    language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    user: Optional["User"] = Relationship(back_populates="books")
+    
+    def __repr__(self):
+        return f"<Book {self.title}>"
+    
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users" #type:ignore
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False), exclude=True
+    )
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    # add this
+    books: List["Book"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    def __repr__(self):
+        return f"<User {self.username}>"
