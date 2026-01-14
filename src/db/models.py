df7f5fd0dffa229ddeaf9a79d6588fffc92a6e34
17Shanto <shahporanshanto23@gmail.com>
2026-01-15 00:44:26 +0600

    feat: add review model

--- src/db/models.py
+++ src/db/models.py
@@ -24,6 +24,9 @@ class Book(SQLModel, table=True):
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     user: Optional["User"] = Relationship(back_populates="books")
+    reviews: List["Review"] = Relationship(
+        back_populates="book", sa_relationship_kwargs={"lazy": "selectin"}
+    )
     
     def __repr__(self):
         return f"<Book {self.title}>"
@@ -53,6 +56,28 @@ class User(SQLModel, table=True):
     books: List["Book"] = Relationship(
         back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
     )
+    reviews: List["Book"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+    
+    
+class Review(SQLModel, table=True):
+    __tablename__ = "reviews" #type: ignore
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    rating: int = Field(le=5)
+    review_text: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
+    book_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="books.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    user: Optional[User] = Relationship(back_populates="reviews")
+    book: Optional[Book] = Relationship(back_populates="reviews")
+
+    def __repr__(self):
+        return f"<Review for book {self.book_uid} by user {self.user_uid}>"
+
+
